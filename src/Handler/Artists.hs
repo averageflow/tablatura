0a050b9049d005c51645471202204199999ae997
Josep Bigorra <josep.bigorra@ingka.ikea.com>
2022-07-16 21:49:32 +0200

    Initial addition of alphabet artists

--- src/Handler/Artists.hs
+++ src/Handler/Artists.hs
@@ -9,10 +9,14 @@ import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
 import Text.Lucius
+import Data.Text (Text, append)
+import Network.HTTP.Types (renderQueryText)
+import Blaze.ByteString.Builder (toByteString)
 
 getArtistsR :: Handler Html
 getArtistsR = do
     defaultLayout $ do
+        let artistLetters = [toUpper [x] | x <- ['a' .. 'z']]
         aDomId <- newIdent
         setTitle "Tablatura"
         $(widgetFile "artists")
