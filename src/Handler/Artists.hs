7c329daad8444b14989fe2055011a2ce4aebe040
Josep Bigorra <josep.bigorra@ingka.ikea.com>
2022-07-17 11:23:09 +0200

    lots of improvements :) understanding more of Yesod

--- src/Handler/Artists.hs
+++ src/Handler/Artists.hs
@@ -6,17 +6,14 @@
 module Handler.Artists where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
-import Text.Lucius
-import Data.Text (Text, append)
-import Network.HTTP.Types (renderQueryText)
-import Blaze.ByteString.Builder (toByteString)
+import Text.Lucius ( luciusFile )
+
 
 getArtistsR :: Handler Html
 getArtistsR = do
     defaultLayout $ do
-        let artistLetters = [toUpper [x] | x <- ['a' .. 'z']]
+        let artistLetters = [toUpper [x] | x <-  ['a' .. 'z']]
+        byLetter <- lookupGetParam "by-letter"
         aDomId <- newIdent
         setTitle "Tablatura"
         $(widgetFile "artists")
